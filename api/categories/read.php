ea61e50afc54763376a81f0f85bae3a4b0f36bdd
Daniel Seip <d_seip@mail.fhsu.edu>
2024-03-16 16:32:46 -0500

    Fixed redundancies in categories API

--- api/categories/read.php
+++ api/categories/read.php
@@ -1,33 +1,12 @@
 <?php
-    // Headers
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json');
-    $method = $_SERVER['REQUEST_METHOD'];
-
-    if ($method === 'OPTIONS') {
-        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
-        exit(); 
-    }
-
-    include_once '../../config/Database.php';
-    include_once '../../models/Category.php';
-
-    // Instantiate DB & connect
-    $database = new Database();
-    $db = $database->connect();
-
-    // Instantiate category object
-    $category = new Category($db);
-
-    // Blog post query
+    // Category query
     $result = $category->read();
     // Get row count
     $num = $result->rowCount();
 
-    // Check if any posts
+    // Check if any categories
     if ($num >0 ) {
-        // Post array
+        // Category array
         $categories_arr = array();
         $categories_arr['data'] = array();
 
@@ -48,6 +27,6 @@
         echo json_encode($categories_arr);
 
     } else {
-        // No posts
+        // No categories
         echo json_encode(array('message' => 'No categories Found'));
     }
--- api/categories/read_single.php
+++ api/categories/read_single.php
@@ -1,29 +1,8 @@
 <?php
-    // Headers
-    header('Access-Control-Allow-Origin: *');
-    header('Content-Type: application/json');
-    $method = $_SERVER['REQUEST_METHOD'];
-
-    if ($method === 'OPTIONS') {
-        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-        header('Access-Control-Allow-Headers: Origin, Accept, Content-Type, X-Requested-With');
-        exit(); 
-    }
-
-    include_once '../../config/Database.php';
-    include_once '../../models/Category.php';
-
-    // Instantiate DB & connect
-    $database = new Database();
-    $db = $database->connect();
-
-    // Instantiate category object
-    $category = new Category($db);
-
     // Get ID
     $category->id = isset($_GET['id']) ? $_GET['id'] : die();
 
-    // Blog post query
+    // Category query
     $category->read_single();
     
     // Create array
